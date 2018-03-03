d77f924de11bc1d3d1825177a317345fc62a7417
DreTaX <dretax@gmail.hu>
2018-03-03 11:26:57 +0100

    1.1

---
+++ AutoDoorCloser/AutoDoorCloser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Fougerite;
+using Fougerite.Events;
+using UnityEngine;
+
+namespace AutoDoorCloser
+{
+    public class AutoDoorCloser : Fougerite.Module
+    {
+        //private MethodInfo togglestateserver;
+        public static int doorLayer;
+
+        public override string Name
+        {
+            get { return "AutoDoorCloser"; }
+        }
+
+        public override string Author
+        {
+            get { return "DreTaX"; }
+        }
+
+        public override string Description
+        {
+            get { return "AutoDoorCloser"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version("1.1"); }
+        }
+
+        public override void Initialize()
+        {
+            Hooks.OnCommand += OnCommand;
+            Hooks.OnDoorUse += OnDoorUse;
+            doorLayer = LayerMask.GetMask(new string[] { "Mechanical" });
+            /*foreach (MethodInfo methodinfo in typeof(BasicDoor).GetMethods((BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic)))
+            {
+                if (methodinfo.Name == "ToggleStateServer")
+                {
+                    if (methodinfo.GetParameters().Length == 3)
+                    {
+                        togglestateserver = methodinfo;
+                        break;
+                    }
+                }
+            }*/
+        }
+
+        public override void DeInitialize()
+        {
+            Hooks.OnCommand -= OnCommand;
+            Hooks.OnDoorUse -= OnDoorUse;
+        }
+
+        public void OnDoorUse(Fougerite.Player p, DoorEvent de)
+        {
+            BasicDoor door = (from collider in Physics.OverlapSphere(p.Location, 3f, doorLayer) where collider.GetComponent<BasicDoor>() select collider.GetComponent<BasicDoor>()).FirstOrDefault();
+            if (DataStore.GetInstance().Get("AutoCloser", p.UID) != null && door != null)
+            {
+                if (door.state.ToString() != "Closed") return;
+                int i = (int) DataStore.GetInstance().Get("AutoCloser", p.UID);
+                var dict = new Dictionary<string, object>();
+                dict["Loc"] = p.Location;
+                dict["Door"] = door;
+                var c = 1000 * i;
+                CreateParallelTimer(c, dict).Start();
+            }
+        }
+
+        public void OnCommand(Fougerite.Player player, string cmd, string[] args)
+        {
+            if (cmd == "doorcloser")
+            {
+                if (args.Length == 0)
+                {
+                    player.Message("Usage: /doorcloser number (1-10)");
+                    return;
+                }
+                int i;
+                string s = string.Join("", args);
+                bool b = int.TryParse(s, out i);
+                if (!b || i > 10 || i < 1)
+                {
+                    player.Message("Usage: /doorcloser number (1-10)");
+                    return;
+                }
+                DataStore.GetInstance().Add("AutoCloser", player.UID, i);
+                player.Message("Closing door after " + i + " seconds");
+            }
+        }
+
+        internal AutoDoorCloserTE CreateParallelTimer(int timeoutDelay, Dictionary<string, object> args)
+        {
+            AutoDoorCloserTE timedEvent = new AutoDoorCloserTE(timeoutDelay);
+            timedEvent.Args = args;
+            timedEvent.OnFire += Callback;
+            return timedEvent;
+        }
+
+        internal void Callback(AutoDoorCloserTE e)
+        {
+            e.Kill();
+            var data = e.Args;
+            var door = (BasicDoor) data["Door"];
+            var loc = (Vector3) data["Loc"];
+            if (door.state.ToString() != "Closing" && door.state.ToString() != "Closed")
+            {
+                door.ToggleStateServer(loc, NetCull.timeInMillis, null);
+            }
+
+            //togglestateserver.Invoke(door, new object[] { loc, NetCull.timeInMillis, null });;
+        }
+    }
+}
---
+++ AutoDoorCloser/AutoDoorCloserTE.cs
@@ -0,0 +1,67 @@
+﻿namespace AutoDoorCloser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Timers;
+
+    public class AutoDoorCloserTE
+    {
+
+        private Dictionary<string, object> _args;
+        private readonly System.Timers.Timer _timer;
+
+        public delegate void TimedEventFireDelegate(AutoDoorCloserTE evt);
+
+        public event TimedEventFireDelegate OnFire;
+
+        public AutoDoorCloserTE(double interval)
+        {
+            this._timer = new System.Timers.Timer();
+            this._timer.Interval = interval;
+            this._timer.Elapsed += new ElapsedEventHandler(this._timer_Elapsed);
+        }
+
+        public AutoDoorCloserTE(double interval, Dictionary<string, object> args)
+            : this(interval)
+        {
+            this.Args = args;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (this.OnFire != null)
+            {
+                this.OnFire(this);
+            }
+
+        }
+
+        public void Start()
+        {
+            this._timer.Start();
+        }
+
+        public void Stop()
+        {
+            this._timer.Stop();
+        }
+
+        public void Kill()
+        {
+            this._timer.Stop();
+            this._timer.Dispose();
+        }
+
+        public Dictionary<string, object> Args
+        {
+            get { return this._args; }
+            set { this._args = value; }
+        }
+
+        public double Interval
+        {
+            get { return this._timer.Interval; }
+            set { this._timer.Interval = value; }
+        }
+    }
+}
